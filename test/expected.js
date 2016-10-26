876b12df30d46d6b929c661c0b920a471af96a47
Greg Banks <gbanks@gmail.com>
2016-10-25 20:14:04 -0700

    add saner error message when commands are not the first argument

--- test/expected.js
+++ test/expected.js
@@ -1,60 +1,111 @@
 var nomnom = require("../nomnom");
 
 var opts = {
-   file: {
-      position: 0,
-      required: true
-   }
-}
+  file: {
+    position: 0,
+    required: true
+  }
+};
 
 var parser = nomnom().options(opts);
 
-exports.testFlag = function(test) {
-   test.expect(1);
+exports.testFlag = function (test) {
+  test.expect(1);
 
-   nomnom().options({
+  nomnom().options({
       file: {
-         position: 0,
+        position: 0,
       }
-   })
-   .printer(function(string) {
-      test.equal(0, string.indexOf("'--key1' expects a value"))
+    })
+    .printer(function (string) {
+      test.equal(0, string.indexOf("'--key1' expects a value"));
       test.done();
-   })
-   .parse(["--key1"]);
-}
+    })
+    .parse(["--key1"]);
+};
 
-exports.testRequired = function(test) {
-   test.expect(1);
+exports.testRequired = function (test) {
+  test.expect(1);
 
-   nomnom().options({
+  nomnom().options({
       file: {
-         required: true
+        required: true
       }
-   })
-   .printer(function(string) {
-      test.equal(0, string.trim().indexOf("file argument is required"))
+    })
+    .printer(function (string) {
+      test.equal(0, string.trim().indexOf("file argument is required"));
       test.done();
-   })
-   .nocolors()
-   .parse([]);
-}
+    })
+    .nocolors()
+    .parse([]);
+};
 
-exports.testChoices = function(test) {
-   test.expect(2);
+exports.testChoices = function (test) {
+  test.expect(2);
 
-   var parser = nomnom().options({
+  var parser = nomnom().options({
       color: {
-         choices: ['green', 'blue']
+        choices: ['green', 'blue']
       }
-   })
-   .printer(function(string) {
+    })
+    .printer(function (string) {
       test.equal(0, string.indexOf("color must be one of: green, blue"))
-   });
+    });
 
-   parser.parse(['--color', 'red']);
+  parser.parse(['--color', 'red']);
 
-   var options = parser.parse(['--color', 'green']);
-   test.equal(options.color, 'green');
-   test.done();
-}
+  var options = parser.parse(['--color', 'green']);
+  test.equal(options.color, 'green');
+  test.done();
+};
+
+exports.testInterspersedCommand = function (test) {
+  //test.expect(3);
+
+  var cbCalled = false;
+
+  var parser = function (printer) {
+    var parser = nomnom().options({
+      foo: {
+        flag: true,
+        help: 'bar'
+      }
+    });
+    parser.command('cmd')
+      .help('fixes all the things')
+      .options({
+        bar: {
+          abbr: 'b',
+          flag: true,
+          help: 'foo'
+        }
+      })
+      .callback(function (opts) {
+        cbCalled = true;
+      });
+    if (printer) {
+      parser.printer(printer);
+    }
+    return parser;
+  };
+
+  var options = undefined;
+
+  test.throws(function() {
+    parser(function (string) {
+        test.equal(0, string.indexOf('commands can not be interspersed with arguments'));
+        throw new Error()
+      }).parse(['--foo', 'cmd', '-b']);
+  }, Error)
+  test.throws(function() {
+    parser(function (string) {
+      test.equal(1, string.indexOf('command argument is required'));
+      throw new Error()
+    }).parse(['--foo', '-b', 'cmd']);
+  }, Error)
+
+  options = parser().parse(['cmd', '--foo', '-b']);
+  test.ok(options.foo);
+  test.ok(options.bar);
+  test.done();
+};
